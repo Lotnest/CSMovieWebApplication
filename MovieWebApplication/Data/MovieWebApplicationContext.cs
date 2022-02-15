e3be6767740566aee98f86552f1571054b219bae
Bloch <michal.bloch@capgemini.com>
2022-02-15 12:43:51 +0100

    Initial commit

---
+++ MovieWebApplication/Data/MovieWebApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieWebApplication.Models;
+
+namespace MovieWebApplication.Data
+{
+    public class MovieWebApplicationContext : DbContext
+    {
+        public MovieWebApplicationContext (DbContextOptions<MovieWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieWebApplication.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MovieWebApplication/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieWebApplication.Data;
+using MovieWebApplication.Models;
+
+namespace MovieWebApplication.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MovieWebApplication.Data.MovieWebApplicationContext _context;
+
+        public DetailsModel(MovieWebApplication.Data.MovieWebApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
